805cbf7fe7358dca0332a456a75ca9705528a4d6
Julien <juju.gruet@mail.com>
2024-10-22 15:57:38 +0200

    fin react quete 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-
-
 import "./App.css";
 
-function App() {
+import MyTitle from "./components/MyTitle";
+import PokemonCard from "./components/PokemonCard/PokemonCard";
 
+function App() {
   return (
-    <>
-    </>
+    <section>
+      <MyTitle />
+      <div>
+        <PokemonCard />
+      </div>
+    </section>
   );
 }
+
 export default App;
